a2d64408e9361e99f59622acc81c44b466d69276
anant1222 <56696253+anant1222@users.noreply.github.com>
2020-02-24 11:20:32 +0530

    script file

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("hello every one how are you i think u all are fine");
+ 
+document.getElementById("heading").addEventListener('mouseover', function(e) {
+    
+    // console.log("You have clicked the heading");
+    // console.log(e);
+    //  let variable;
+    //  variable=e.target;
+    //  variable=Array.from(e.target.classList);
+    //  console.log(variable);
+     document.body.style.backgroundColor=`rgb(${e.offsetX},${e.offsetX},${e.offsetX},${e.offsetX},${e.offsetX}154)`;
+    
+    // location.href="//google.com"
+   
+})
+
+
+// console.log('e');
